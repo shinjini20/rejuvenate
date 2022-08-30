8ec24ec5822cc8ae6d7a71f4add3f892ede35680
shinjini20 <110297930+shinjini20@users.noreply.github.com>
2022-08-30 22:51:06 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+// const toggleButton = document.getElementsByClassName('toggle-button')[0]
+// const navbarLinks = document.getElementsByClassName('navbar-links')[0]
+
+// toggleButton.addEventListener('click', () => {
+//   navbar-links.classList.togglebutton('active')
+// })
+
+function onClickMenu(){
+  document.getElementById("menu").classList.toggle("icon");
+  document.getElementById("nav").classList.toggle("change");
+}
